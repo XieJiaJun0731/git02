8c5c2467fac3621a9ef918298ee08ec8aca300d1
“XIEJIAJUN” <1291007485@qq.com>
2021-10-07 23:12:39 +0800

    主干操作

--- src/test/java/com/xxxx/AppTest.java
+++ src/test/java/com/xxxx/AppTest.java
@@ -16,5 +16,6 @@ public class AppTest
     public void testabc(){
         System.out.println("abc.......");
         System.out.println("分支操作.......");
+        System.out.println("master操作.......");
     }
 }
